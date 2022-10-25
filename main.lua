ee3cadb932966e5bb7208bc8ab80eac4b1cf3a43
Yomobi827 <104099593+Yomobi827@users.noreply.github.com>
2022-10-25 16:43:14 +0100

    new error codes

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ while task.wait(3) do
 	for i, v in players do
 		if v.Character:FindFirstChild("Highlight") then
 			warn("ESP Detected")
-            game.Players.LocalPlayer:Kick("Anticheat-Trigger: ESP | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+            game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code E1")
 		end
 	end
 	for i,v in (game.Players.LocalPlayer:GetChildren()) do
@@ -12,39 +12,39 @@ while task.wait(3) do
 			local Module = require(Setting)
 			if Module.AmmoPerMag >= 999 then
 				warn("InfAmmo Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InfAmmo | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 1a")
 			end
 			if Module.Ammo >= 999 then
 				warn("InfAmmo Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InfAmmo | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 1b")
 			end
 			if Module.MaxAmmo >= 999 then
 				warn("InfAmmo Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InfAmmo | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 1c")
 			end
 			if Module.LimitedAmmoEnabled == 999 then
 				warn("InfAmmo Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InfAmmo | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 1d")
 			end
 			if Module.BaseDamage >= 999 then
 				warn("OneShot Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: OneShot | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 2a")
 			end
 			if Module.Spread <= 0 then
                 warn("NoSpread Detected")
-				game.Players.LocalPlayer:Kick("Anticheat-Trigger: NoSpread | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 3a")
 			end
 			if Module.Recoil <= 0 then
 				warn("NoRecoil Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: NoRecoil | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 4a")
 			end
 			if Module.ReloadTime <= 0 then
 				warn("InstantReload Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InstantReload | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 5a")
 			end
 			if Module.EquipTime <= 0 then
 				warn("InstantEquip Detected")
-                game.Players.LocalPlayer:Kick("Anticheat-Trigger: InstantEquip | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+                game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code 6a")
 			end
 			
 		end
@@ -56,7 +56,7 @@ while task.wait(20) do
 	for i, v in ws do
 		if v.Transparency == 0.5 and v.BrickColor == BrickColor.new("Lime green") then
 			warn("ESP Detected")
-            game.Players.LocalPlayer:Kick("Anticheat-Trigger: ESP | This kick was made by a prototype anticheat and may be a false positive, you have not been banned")
+			game.Players.LocalPlayer:Kick("Anticheat-Trigger: Code E2")
 		end
 	end
 end
