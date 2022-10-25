1a432b920271d41e8039e7d4d1f0f4f6e416478e
Yomobi827 <104099593+Yomobi827@users.noreply.github.com>
2022-10-25 21:28:42 +0100

    Add files via upload

---
+++ serverside.lua
@@ -0,0 +1,11 @@
+game.ReplicatedStorage.Events.Anticheat.PlayerBanned.OnServerEvent:Connect(function(plr, code)
+    warn(plr, "was banned for code", code, "checking validity")
+    local PlayerList = game.Players:GetChildren()
+    for i, v in PlayerList do
+        if v == plr then
+            warn("AntiCheatServerSide Error Code 1, contact developers immedietly")
+            return "AntiCheatServerSide Error Code 1"
+        end
+    end
+    -- add to banlist
+end)
